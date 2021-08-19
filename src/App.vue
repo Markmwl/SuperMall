6146f307733f19a68ed9b962007a14d7727b029c
Mark <528414865@qq.com>
2021-08-19 09:51:17 +0800

    删除原始文件HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
   <div id="app">
 	  <h4>Hello Markmwl111222!</h4>
-	  <a>git.exe push -v --progress "SuperMall" main:main</a>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ @import url("assets/css/base.css");
 </style>
